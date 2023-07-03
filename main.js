e2fe2a1e9236cc775d2cf06ef68bc7f23113447a
Maylen <maylendeulofeu@gmail.com>
2023-07-03 18:02:16 -0400

    Create a function that creates divs using loop

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
 const container = document.createElementById('container');
+function createDivs(gridSize) {
+    for (let i = 0; i < gridSize; i++) {
+        const div = document.createElement('div');
+        gridSize = 256;
+        container.appendChild(div);
+    }
+}
