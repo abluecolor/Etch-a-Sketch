4f40ec1126779813906d528ea35dd087c6bc4d5e
Maylen <maylendeulofeu@gmail.com>
2023-07-03 19:23:29 -0400

    Change placement of declarations for cleaner code

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 const container = document.getElementById('container');
+const gridSize = 256;
+
 function createDivs(gridSize) {
     for (let i = 0; i < gridSize; i++) {
         const div = document.createElement('div');
         container.appendChild(div)
     }
 }
-
-const gridSize = 256;
 createDivs(gridSize);
