6a3f797faa9d18b26d66431da5cf5ce1ccd91bdb
Maylen <maylendeulofeu@gmail.com>
2023-07-04 18:14:10 -0400

    Add new HTML elements Change CSS selector to .square to fix specificity error

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
-const container = document.getElementById('container');
+const board = document.getElementById('board');
 const gridSize = 256;
 
 function createSquares(gridSize) {
     for (let i = 0; i < gridSize; i++) {
         const square = document.createElement('div');
-        container.appendChild(square);
+        board.appendChild(square);
     }
 }
 createSquares(gridSize);
