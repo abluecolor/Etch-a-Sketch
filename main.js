eb24aa711525d2ad021c73287fefdb16f6b1be88
Maylen <maylendeulofeu@gmail.com>
2023-07-04 18:56:46 -0400

    Add css class square to the square element and fix issue

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const gridSize = 256;
 function createSquares(gridSize) {
     for (let i = 0; i < gridSize; i++) {
         const square = document.createElement('div');
+        square.classList.add('square');
         board.appendChild(square);
     }
 }
