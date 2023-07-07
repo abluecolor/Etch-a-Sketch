b5d3eff1d13e882588b2a0640785a16af47fa98e
Maylen <maylendeulofeu@gmail.com>
2023-07-06 20:05:54 -0400

    Create first few elements using JavaScript

--- main.js
+++ main.js
@@ -1,6 +1,18 @@
 const board = document.getElementById('board');
 const gridSize = 256;
 
+const flexContainer = document.createElement('div');
+flexContainer.classList.add('flexContainer');
+document.body.appendChild(flexContainer);
+
+const titleDiv = document.createElement('div');
+titleDiv = classList.add('titleDiv');
+flexContainer.appendChild(titleDiv);
+
+const heading = document.createElement('h1');
+heading = classList.add('heading');
+titleDiv.appendChild(heading);
+
 function createSquares(gridSize) {
     for (let i = 0; i < gridSize; i++) {
         const square = document.createElement('div');
